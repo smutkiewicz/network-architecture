5868ff11384aebd9a3269209d5503589640f75dc
Tuptaju <tuptaju@gmail.com>
2017-11-26 10:47:19 +0100

    Floyd (kolorowanie grafu)

--- src/com/company/algorithm/FloydAlgorithm.java
+++ src/com/company/algorithm/FloydAlgorithm.java
@@ -17,20 +17,11 @@ public class FloydAlgorithm implements MyAlgorithm {
             floyd(network, inputPath);
         }
 
-        return network;
-    }
-
-    public Network floyd(Network network, InputPath inputPath) {
-
-        int start = inputPath.start - 1;
-        int end = inputPath.end - 1;
         int verticesAmount = network.getVerticesArray().size();
         int p[][]= new int [verticesAmount+1][verticesAmount+1];
-
         double d[][]= new double [verticesAmount+1][verticesAmount+1];
-
-        ArrayList<Link> links = network.getLinksArray();
-        ArrayList<Vertex> vertices = network.getVerticesArray();
+        int i=0, j=0, k=0;
+        Vertex vertex;
 
         //wypełniam macierz nieskończonościami
         for (double[] row: d)
@@ -39,26 +30,20 @@ public class FloydAlgorithm implements MyAlgorithm {
         for (int[] row: p)
             Arrays.fill(row, -1);
 
-        int i=0, j=0, k=0;
-
         //wypełniam przekątną zerami, bo droga A->A =0
         for (i = 0; i < verticesAmount+1; i++)
             d[i][i]=0;
 
-        Vertex vertex;
         i=1;
         j=1;
         //wypełniam kolejno istniejące połączenia węzłów wagami
         for (i = 1; i < verticesAmount+1; i++) {
-            //links = network.getVertex(i).getLinksList();
-            //vertex=network.getVertex(i);
-            //System.out.println(vertices.get(i).getId());
             for (j = 1; j < verticesAmount+1; j++) {
                 if (i==j)
                     continue;
                 else if (network.checkIfLinked(network.getVertex(i), network.getVertex(j))){
-                d[i][j] = network.getLink(i, j).getWeight();
-                p[i][j] = i;
+                    d[i][j] = network.getLink(i, j).getWeight();
+                    p[i][j] = i;
                 }
             }
         }
@@ -73,10 +58,10 @@ public class FloydAlgorithm implements MyAlgorithm {
             System.out.println();
             }
 */
-        //k->k, u->j, v->i
-        for (k = 0; k < verticesAmount; k++) {
-            for (j = 0; j < verticesAmount; j++){
-                for (i = 0; i < verticesAmount; i++){
+
+        for (k = 1; k < verticesAmount+1; k++) {
+            for (j = 1; j < verticesAmount+1; j++){
+                for (i = 1; i < verticesAmount+1; i++){
                     if (d[j][i]>d[j][k]+d[k][i]) {
                         d[j][i] = d[j][k] + d[k][i];
                         p[j][i] = p[k][i];
@@ -101,6 +86,55 @@ public class FloydAlgorithm implements MyAlgorithm {
             }
             System.out.println();
         }
+
+        boolean skierowanie = false;
+
+        i=2;
+        j=1;
+        k=j;
+        int path=0;
+        if (i == j)
+            System.out.println("debilu, to ten sam węzeł");
+        else if (p[i][j] == -1)
+            System.out.println("nie ma połączenia");
+        else {
+            path++;
+
+            if (!skierowanie)
+                if (d[i][j] < d[j][i]) {
+                    System.out.println(d[i][j]);
+                    while (i != j) {
+                        network.colorLink(j, p[i][j], path);
+                        //System.out.println("pierwszy if " + j + " ");
+                        j = p[i][j];
+                    }
+                    network.colorLink(j, p[i][j], path);
+                } else {
+                    System.out.println(d[j][i]);
+                    while (i != j) {
+                        network.colorLink(i, p[j][i], path);
+                        //System.out.println("drugi if " + i + " ");
+                        i = p[j][i];
+                    }
+                    network.colorLink(j, p[i][j], path);
+                }
+            else {
+                System.out.println(d[i][j]);
+                while (i != j) {
+                    network.colorLink(j, p[i][j], path);
+                    //System.out.println("trzeci if " + j + " ");
+                    j = p[i][j];
+                }
+                network.colorLink(j, p[i][j], path);
+            }
+        }
+        return network;
+    }
+
+    public Network floyd(Network network, InputPath inputPath) {
+
+        int start = inputPath.start - 1;
+        int end = inputPath.end - 1;
 
         return network;
     }
